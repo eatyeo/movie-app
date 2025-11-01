901083f096ad17c7a0466ea2c002550ad068896b
emily <emilyatyeo44@gmail.com>
2025-11-01 00:03:00 -0400

    feat: add loading spinner

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ const App = () => {
   const [searchTerm, setSearchTerm] = useState('')
   const [errorMessage, setErrorMessage] = useState('');
   const [movieList, setMovieList] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
 
   const fetchMovies = async (query) => {
     setIsLoading(true);
@@ -45,7 +45,7 @@ const App = () => {
   } catch (error) {
       console.error(`Error fetching movies. Please try again later.`);
   } finally {
-    setIsLoading(true);
+    setIsLoading(false);
   }
 
   useEffect(() => {
@@ -63,10 +63,10 @@ const App = () => {
         </header>
 
         <section className="all-movies">
-          <h2>All Movies</h2>
+          <h2 className="mt-[40px]">All Movies</h2>
 
           {isLoading ? (
-            <p className="text-white">Loading...</p>
+            <Spinner />
           ) : errorMessage ? (
           <p className="text-red-500">{errorMessage}</p>
           ) : (
